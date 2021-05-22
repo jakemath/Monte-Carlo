8561d48daf8183600263dc6b0425f981a947c4cf
Jake Mathai <jakemathai5@gmail.com>
2021-05-22 00:29:34 -0700

    Update routines.cpp

--- routines.cpp
+++ routines.cpp
@@ -12,21 +12,18 @@ double stock_price(double S_t,
                    double mu,
                    double sigma,
                    double delta_t,
-                   std::normal_distribution<double> norm)
-{
+                   std::normal_distribution<double> norm) {
     std::random_device rd;
     return S_t*(1.0 + mu*delta_t + sigma*norm(rd)*pow(delta_t, 0.5));
 }
 
 
-list<double> generate_price_path(double S_0, double mu, double sigma, double delta_t, int n)
-{
+list<double> generate_price_path(double S_0, double mu, double sigma, double delta_t, int n) {
     list<double> path;
     double S_t = S_0;
     path.push_back(S_t);
     std::normal_distribution<double> norm(mu, sigma);
-    for (unsigned i = 1; i <= n; ++i)
-    {
+    for (unsigned i = 1; i <= n; ++i) {
         path.push_back(stock_price(S_t, mu, sigma, delta_t, norm));
         S_t = path.back();
     }
@@ -34,11 +31,9 @@ list<double> generate_price_path(double S_0, double mu, double sigma, double del
 }
 
 
-list<list<double>> simulate_paths(double S_0, double mu, double sigma, double delta_t, int n, int m)
-{
+list<list<double>> simulate_paths(double S_0, double mu, double sigma, double delta_t, int n, int m) {
     list<list<double>> paths;
-    for (unsigned i = 1; i <= m; ++i)
-    {
+    for (unsigned i = 1; i <= m; ++i) {
         cout << "PATH " << i << endl;
         paths.push_back(generate_price_path(S_0, mu, sigma, delta_t, n));
     }
@@ -46,8 +41,7 @@ list<list<double>> simulate_paths(double S_0, double mu, double sigma, double de
 }
 
 
-std::ostream& operator << (std::ostream& o, const list<double>& l)
-{
+std::ostream& operator << (std::ostream& o, const list<double>& l) {
     list<double>::const_iterator i = l.begin();
     for (; i != --l.end(); ++i)
         o << *i << ", ";
