8d3b077393a816ce03c264f692337302a61470ed
Jake Mathai <jakemathai5@gmail.com>
2021-05-22 00:30:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@
 
 #include "routines.h"
 
-int main()
-{
+int main() {
     list<list<double>> paths = simulate_paths(100.0, 0.0, .1, 0.001, 10000, 20);
     std::ofstream out("paths.txt");
     for (list<list<double>>::iterator i = paths.begin(); i != paths.end(); ++i)
